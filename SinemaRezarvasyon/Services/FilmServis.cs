971f9e18aed5ffd9e4e0092fa1d56475cacd0793
beyzamrtg <132819496+beyzamrtg@users.noreply.github.com>
2024-04-16 14:24:16 +0300

    Proje dosyası ekle.

---
+++ SinemaRezarvasyon/Services/FilmServis.cs
@@ -0,0 +1,25 @@
+﻿using SinemaRezarvasyon.Data;
+using SinemaRezarvasyon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaRezarvasyon.Services
+{
+    public class FilmServis
+    {
+        AppDbContext _dbContext = new AppDbContext();
+
+        public List<Film> GetAll()
+        {
+            return _dbContext.Film.ToList();
+        }
+
+        public Film GetFilmDetailsBasedOnfilmId(int filmId)
+        {
+            return _dbContext.Film.FirstOrDefault(m => m.id == filmId);
+        }
+    }
+}
---
+++ SinemaRezarvasyon/Services/SeansServis.cs
@@ -0,0 +1,39 @@
+﻿using SinemaRezarvasyon.Data;
+using SinemaRezarvasyon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SinemaRezarvasyon.Services
+{
+   public class SeansServis
+    {
+        AppDbContext _dbContext = new AppDbContext();
+
+        public List<Seans> GetAll()
+        {
+            return _dbContext.Seans.ToList();
+        }
+        public List<Seans> FilmleriSeansaGoreListele(string filmAdi)
+        {
+            var seanslar = _dbContext.Seans
+                                     .Join(_dbContext.Film,
+                                           seans => seans.filmId,
+                                           film => film.id,
+                                           (seans, film) => new { Seans = seans, FilmAdi = film.filmAdi })
+                                     .Where(x => x.FilmAdi == filmAdi)
+                                     .Select(x => x.Seans)
+                                     .ToList();
+
+            return seanslar;
+        }
+        public Seans GetSeansDetailsBasedOnseansId(int seansId)
+        {
+            return _dbContext.Seans.FirstOrDefault(s => s.id == seansId);
+        }
+    }
+
+}
